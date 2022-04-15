65d83eae5a317aaab771df6c3f18e951ebf35fbc
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-04-15 17:16:15 -0300

    rutas y sidebar

--- src/components/Login/Register.jsx
+++ src/components/Login/Register.jsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import axios from 'axios';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Header from "../LayoutPublic/Header/Header";
+// import axios from 'axios';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Pilsen Digital
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 const useStyles = makeStyles((theme) => ({
-  root:{
-    background:"white",
-    borderRadius:"5px",
+  root: {
+    background: "white",
+    borderRadius: "5px",
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', 
+    width: "100%",
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -50,102 +51,104 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const APIurl= 'http://localhost:3001/api/auth/register';
-
+// const APIurl = "http://localhost:3001/api/auth/register";
 
 export default function SignIn() {
   const classes = useStyles();
   const [datos, setDatos] = useState({
-     "name": "",
-     "email": "",
-     "password": ""
-  }
-  )
+    name: "",
+    email: "",
+    password: "",
+  });
 
   const handleInputChange = (e) => {
     setDatos({
-         ...datos,
-         [e.target.name]: e.target.value
-    })
-  }
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  async function handleSubmit(){
+  async function handleSubmit() {
     // const req = await axios.post(`${APIurl}`,datos);
     console.log(datos);
   }
 
   return (
-    <Container component="main" maxWidth="xs" className={classes.root}>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Registrate
-        </Typography>
-        <form className={classes.form} Validate>
-        <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="name"
-            label="Nombre"
-            name="name"
-            autoComplete="nombre"
-            onChange={handleInputChange}
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            onChange={handleInputChange}
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Contraseña"
-            type="password"
-            onChange={handleInputChange}
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Recuerdame"
-          />
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={() => handleSubmit()}
-          >
-            Acceder
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="/login" variant="body2">
-                {"Ya tienes cuenta? Inicia sesion"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8} mb={8}>
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      <Header>
+        <Container component="main" maxWidth="xs" className={classes.root}>
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Registrate
+            </Typography>
+            <form className={classes.form} Validate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Nombre"
+                name="name"
+                autoComplete="nombre"
+                onChange={handleInputChange}
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                onChange={handleInputChange}
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Contraseña"
+                type="password"
+                onChange={handleInputChange}
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Recuerdame"
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={() => handleSubmit()}
+              >
+                Acceder
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="/login" variant="body2">
+                    {"Ya tienes cuenta? Inicia sesion"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+          <Box mt={8} mb={8}>
+            <Copyright />
+          </Box>
+        </Container>
+      </Header>
+    </div>
   );
 }
